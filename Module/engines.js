bfb3ff832d0c9f471195c73feda92dbba97ca6d1
Ayfri <pierre.ayfri@gmail.com>
2021-01-01 06:44:13 +0100

    Updated discord.js to 12.5.1, now using dayjs instead of moment.

--- Module/engines.js
+++ Module/engines.js
@@ -1,68 +1,69 @@
-
-const moment = require('moment')
+const dayjs = require('dayjs');
 
 // Moteur de test et de conversion des arguments
 const processors = {
-    'bool' : arg => /(?:true|false)/i.test(arg) ? /true/i.test(arg) : 'invalid',
-    'url' : arg => /(?:https?:\/\/(?:www\.|(?!www))[a-z\d][a-z\d-]+[a-z\d]\.[^\s]{2,}|www\.[a-z\d][a-z\d-]+[a-z\d]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-z\d]+\.[^\s]{2,}|www\.[a-z\d]+\.[^\s]{2,})/i.test(arg) ? arg : 'invalid',
-    'img' : arg => /\S+\.(?:jpg|jpeg|gif|png|bmp|webp)/i.test(arg) ? arg : 'invalid',
-    '2048' : arg => arg.length > 0 ? arg.slice(0,2048) : 'invalid',
-    '1024' : arg => arg.length > 0 ? arg.slice(0,1024) : 'invalid',
-    '256' : arg => arg.length > 0 ? arg.slice(0,256) : 'invalid',
-    'date' : arg => /now/i.test(arg) ? Date.now() : !isNaN(Number(arg)) ? moment(Number(arg)).isValid() ? Number(arg) : 'invalid' : moment(arg).isValid() ? moment(arg).valueOf() : 'invalid',
-    'color' : arg => /(?:(?:0x){0,1}|#{0,1})(?:[0-9A-F]{8}|[0-9A-F]{6})/i.test(arg) ? arg.startsWith('#') ? arg : Number(arg) : 'invalid'
-}
+	'bool':  arg => /(?:true|false)/i.test(arg) ? /true/i.test(arg) : 'invalid',
+	'url':   arg => /(?:https?:\/\/(?:www\.|(?!www))[a-z\d][a-z\d-]+[a-z\d]\.[^\s]{2,}|www\.[a-z\d][a-z\d-]+[a-z\d]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-z\d]+\.[^\s]{2,}|www\.[a-z\d]+\.[^\s]{2,})/i.test(arg) ?
+	                arg :
+	                'invalid',
+	'img':   arg => /\S+\.(?:jpg|jpeg|gif|png|bmp|webp)/i.test(arg) ? arg : 'invalid',
+	'2048':  arg => arg.length > 0 ? arg.slice(0, 2048) : 'invalid',
+	'1024':  arg => arg.length > 0 ? arg.slice(0, 1024) : 'invalid',
+	'256':   arg => arg.length > 0 ? arg.slice(0, 256) : 'invalid',
+	'date':  arg => /now/i.test(arg) ? Date.now() : !Number.isNaN(Number(arg)) ? dayjs(Number(arg)).isValid() ? Number(arg) : 'invalid' : dayjs(arg).isValid() ? dayjs(arg).valueOf() : 'invalid',
+	'color': arg => /(?:(?:0x)?|#?)(?:[0-9A-F]{8}|[0-9A-F]{6})/i.test(arg) ? arg.startsWith('#') ? arg : Number(arg) : 'invalid',
+};
 
 // Règlement des balises
 const tags = {
-    title : {
-        args : ['256'],
-        method : 'setTitle'
-    },
-    author : {
-        args : ['256','?img','?url'],
-        method : 'setAuthor'
-    },
-    description : {
-        args : ['2048'],
-        method : 'setDescription'
-    },
-    footer : {
-        args : ['2048','?img'],
-        method : 'setFooter'
-    },
-    url : {
-        args : ['url'],
-        method : 'setURL'
-    },
-    image : {
-        args : ['img'],
-        method : 'setImage'
-    },
-    thumbnail : {
-        args : ['img'],
-        method : 'setThumbnail'
-    },
-    field : {
-        args : ['256','1024','?bool'],
-        method : 'addField'
-    },
-    timestamp : {
-        args : ['date'],
-        method : 'setTimestamp',
-    },
-    color : {
-        args : ['color'],
-        method : 'setColor'
-    }
-}
+	title:       {
+		args:   ['256'],
+		method: 'setTitle',
+	},
+	author:      {
+		args:   ['256', '?img', '?url'],
+		method: 'setAuthor',
+	},
+	description: {
+		args:   ['2048'],
+		method: 'setDescription',
+	},
+	footer:      {
+		args:   ['2048', '?img'],
+		method: 'setFooter',
+	},
+	url:         {
+		args:   ['url'],
+		method: 'setURL',
+	},
+	image:       {
+		args:   ['img'],
+		method: 'setImage',
+	},
+	thumbnail:   {
+		args:   ['img'],
+		method: 'setThumbnail',
+	},
+	field:       {
+		args:   ['256', '1024', '?bool'],
+		method: 'addField',
+	},
+	timestamp:   {
+		args:   ['date'],
+		method: 'setTimestamp',
+	},
+	color:       {
+		args:   ['color'],
+		method: 'setColor',
+	},
+};
 
 // Texte de la regex globale
-const regexString = `(?:\\s|^)\\$@tag\\s+([\\w\\W]*?)\\s+\\$(?:end|${Object.keys(tags).join('|')})(?:\\s|$)`
+const regexString = `(?:\\s|^)\\$@tag\\s+([\\w\\W]*?)\\s+\\$(?:end|${Object.keys(tags).join('|')})(?:\\s|$)`;
 
 // Exportation
 module.exports = {
-    regexString : regexString,
-    processors : processors,
-    tags : tags
-}
+	regexString: regexString,
+	processors:  processors,
+	tags:        tags,
+};
--- Module/index.js
+++ Module/index.js
@@ -1,96 +1,91 @@
+const {
+	processors,
+	tags,
+	regexString,
+} = require('./engines.js');
+const {MessageEmbed} = require('discord.js');
 
-const { processors, tags, regexString } = require( "./engines.js" )
-const { RichEmbed } = require( "discord.js" )
-
-function toEmbed( string ){
-
-    let source = string.replace( /[^:]\$blank/ig, '\u200B' ) + '\n$end'
-    
-    const embed = new RichEmbed()
-    const errors = []
-
-    // Pour chaque balise existente
-    for( tag in tags ){
-
-        const { args, method } = tags[ tag ]
-        const types = args.map( arg => arg.replace( '?', '' ) )
-        const regex = new RegExp( regexString.replace( '@tag', tag ), 'im' )
-        
-        // Pour chaque occurence de cette balise
-        while( regex.test( source ) ){
-            
-            let argContents
-            const logArgs = []
-            const valids = []
-            const andTagRegex = /\s+\$(?:and|&)\s+/i
-            let [ fullmatch, content ] = regex.exec( source )
-
-            // Effacer la balise traitée de "source"
-            source = source.replace( new RegExp( `(?:^|\\s)\\$${tag}(?:$|\\s)`, 'i' ), ' $end ' )
-            content = content.trim()
-
-            if( args.length == 1 ){
-                argContents = [ content ]
-            }else{
-                if( andTagRegex.test( content ) ){
-                    argContents = content.split( andTagRegex )
-                }else{
-                    argContents = content.split( /\s*[\r\n]+\s*/ )
-                }
-            }
-
-            delete fullmatch
-            delete content
-
-            // Pour chaque argument demandé par la méthode
-            types.forEach(( type, index ) => {
-
-                const fullArg = args[ index ]
-                let arg = argContents[ index ]
-
-                // Si l'argument n'est pas null ou "null"
-                if( arg && !/^\$null$/i.test( arg ) ){
-
-                    // Convertir l'argument
-                    const valid = processors[ type ]( arg )
-
-                    // Injection de validité
-                    if( valid === 'invalid' ){
-                        logArgs.push( 'invalid: ' + type )
-                    }else{
-                        logArgs.push( 'valid: ' + type )
-                        valids[ index ] = valid
-                    }
-                }else{
-
-                    // Injection de nullité
-                    if( fullArg.startsWith( '?' ) ){
-                        logArgs.push( 'omit: ' + type )
-                        valids[ index ] = null
-                    }else{
-                        logArgs.push( 'forget: ' + type )
-                        valids[ index ] = 'GodArgumentError'
-                    }
-                }
-            })
-
-            // Si les arguments obligatoires sont présents
-            if( !valids.find(( arg ) => arg === 'GodArgumentError') ){
-                // Ajouter les arguments à l'embed
-                embed[ method ]( ...valids )
-
-            // Sinon
-            }else{
-                // Ajouter une erreur dans le log d'erreurs
-                errors.push( `Missing argument(s): ${ method }( ${ logArgs.join( ', ' ) } )` )
-            }
-        }
-    }
-
-    return {
-        embed : embed,
-        errors : errors
-    }
+function toEmbed(string) {
+	
+	let source = string.replace(/[^:]\$blank/ig, '\u200B') + '\n$end';
+	
+	const embed = new MessageEmbed();
+	const errors = [];
+	
+	// Pour chaque balise existente
+	for (let tag in tags) {
+		
+		const {
+			args,
+			method,
+		} = tags[tag];
+		const types = args.map(arg => arg.replace('?', ''));
+		const regex = new RegExp(regexString.replace('@tag', tag), 'im');
+		
+		// Pour chaque occurence de cette balise
+		while (regex.test(source)) {
+			
+			let argContents;
+			const logArgs = [];
+			const valides = [];
+			const andTagRegex = /\s+\$(?:and|&)\s+/i;
+			let [_, content] = regex.exec(source);
+			
+			// Effacer la balise traitée de "source"
+			source = source.replace(new RegExp(`(?:^|\\s)\\$${tag}(?:$|\\s)`, 'i'), ' $end ');
+			content = content.trim();
+			
+			argContents = args.length === 1 ? [content] : andTagRegex.test(content) ? content.split(andTagRegex) : content.split(/\s*[\r\n]+\s*/);
+			
+			// Pour chaque argument demandé par la méthode
+			types.forEach((type, index) => {
+				
+				const fullArg = args[index];
+				let arg = argContents[index];
+				
+				// Si l'argument n'est pas null ou "null"
+				if (arg && !/^\$null$/i.test(arg)) {
+					
+					// Convertir l'argument
+					const valid = processors[type](arg);
+					
+					// Injection de validité
+					if (valid === 'invalid') {
+						logArgs.push('invalid: ' + type);
+					} else {
+						logArgs.push('valid: ' + type);
+						valides[index] = valid;
+					}
+				} else {
+					
+					// Injection de nullité
+					if (fullArg.startsWith('?')) {
+						logArgs.push('omit: ' + type);
+						valides[index] = null;
+					} else {
+						logArgs.push('forget: ' + type);
+						valides[index] = 'GodArgumentError';
+					}
+				}
+			});
+			
+			// Si les arguments obligatoires sont présents
+			if (!valides.find((arg) => arg === 'GodArgumentError')) {
+				// Ajouter les arguments à l'embed
+				embed[method](...valides);
+				
+				// Sinon
+			} else {
+				// Ajouter une erreur dans le log d'erreurs
+				errors.push(`Missing argument(s): ${method}( ${logArgs.join(', ')} )`);
+			}
+		}
+	}
+	
+	return {
+		embed:  embed,
+		errors: errors,
+	};
 }
 
-module.exports = toEmbed
+module.exports = toEmbed;
